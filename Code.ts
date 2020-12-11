7672b8e7c1433608e445e4a625aabf4a5c00c500
becky <beckymccarthy777@gmail.com>
2020-12-11 12:58:22 -0800

    Email confirmation and logging status code
    completely working and updating sheet accordingly.

--- Code.ts
+++ Code.ts
@@ -3,16 +3,17 @@ const onFormSubmit = (event: GoogleAppsScript.Events.SheetsOnFormSubmit) => {
   Logger.log(JSON.stringify(event));
   Logger.log(event.namedValues)
   var sheet = SpreadsheetApp.getActiveSheet();
+  var rowIndex = event.range.getLastRow();
   // let values = [['TRUE', new Date()]];
   // sheet.getRange(event.range.getLastRow(), 4, 1, 2).setValues(values);
-  var name = sheet.getRange(event.range.getLastRow(), 2).getValue();
-  var email = sheet.getRange(event.range.getLastRow(), 3).getValue();
-  var city = sheet.getRange(event.range.getLastRow(), 5).getValue();
-  var reason = sheet.getRange(event.range.getLastRow(), 6).getValue();
-  const userInfo = {name: name, email: email, city: city, reason: reason};
+  var name = sheet.getRange(rowIndex, 2).getValue();
+  var email = sheet.getRange(rowIndex, 3).getValue();
+  var city = sheet.getRange(rowIndex, 5).getValue();
+  var reason = sheet.getRange(rowIndex, 6).getValue();
+  const userInfo = {name, email, city, reason};
   Logger.log('userInfo: ', userInfo);
-  postToLob(userInfo);
-  sendConfirmationEmail();
+  postToLob(userInfo, rowIndex);
+  sendConfirmationEmail(userInfo, rowIndex);
 };
 
 const createOnFormSubmitTrigger = () => {
@@ -23,28 +24,20 @@ const createOnFormSubmitTrigger = () => {
 };
 
 var EMAIL_SENT = 'EMAIL_SENT';
-const sendConfirmationEmail = () => {
+const sendConfirmationEmail = (userInfo, rowIndex) => {
   var sheet = SpreadsheetApp.getActiveSheet();
-  var startRow = 2;
-  var numRows = 2;
-  var dataRange = sheet.getRange(startRow, 1, numRows, 10);
-  var data = dataRange.getValues();
-  for (var i = 0; i < data.length; ++i) { 
-    var row = data[i];
-    var email = row[2];
-    var name = row[1];
-    var message = "Thank you for participating " + name + ". A postcard will be created in your name and delivered to the legislators.";
-    var email_sent = row[9];
+    var message = "Thank you for participating " + userInfo.name + ". A postcard will be created in your name and delivered to the legislators.";
+    var email_sent = sheet.getRange(rowIndex, 10).getValue();
     if (email_sent !== EMAIL_SENT) {
       var subject = "Reframing Justice Project";
-      MailApp.sendEmail(email, subject, message);
-      sheet.getRange(startRow + i, 10).setValue(EMAIL_SENT);
+      MailApp.sendEmail(userInfo.email, subject, message);
+      sheet.getRange(rowIndex, 10).setValue(EMAIL_SENT);
       SpreadsheetApp.flush();
     };
   }
 };
 
-const postToLob = (userInfo) => {
+const postToLob = (userInfo, rowIndex) => {
   let url = "https://api.lob.com/v1/postcards";
   let API_KEY = "test_7116a24dd8a038acadc7bbed8eff2c55168";
   let data = {
@@ -54,7 +47,13 @@ const postToLob = (userInfo) => {
     front: '<html style="padding: 1in; font-size: 50;"></html>',
     back: "tmpl_3cdf2f9422f2968",
     // '<html style="padding: 1in; font-size: 20;">{{city}} {{reason}} {{name}} {{email}}</html>',
+    
+    
     merge_variables: {
+      // name: 'Harry',
+      // email: 'me@email.com',
+      // city: 'Tuscon',
+      // reason: 'too expensive',
       name: userInfo.name,
       email: userInfo.email,
       city: userInfo.city,
@@ -69,11 +68,24 @@ const postToLob = (userInfo) => {
       Authorization: "Basic " + Utilities.base64Encode(API_KEY + ":"),
       
     },
-    payload: JSON.stringify(data)
+    payload: JSON.stringify(data),
+    muteHttpExceptions: true
+    
   };
   Logger.log(data);
-  // @ts-ignore
-  let response = UrlFetchApp.fetch(url, options);
-  Logger.log(response.getContentText());
+  try{
+     // @ts-ignore
+    let response = UrlFetchApp.fetch(url, options);
+    var responseCode = response.getResponseCode();
+    var responseBody = response.getContentText();
+    var sheet = SpreadsheetApp.getActiveSheet();
+    sheet.getRange(rowIndex, 11).setValue(responseCode);
+    Logger.log('response: ', response);
+    Logger.log('responseCode: ', responseCode);
+    Logger.log('responseBody: ', responseBody);
+  } catch (error) {
+    Logger.log('error: ', error);
+  }
+ 
 };
 
