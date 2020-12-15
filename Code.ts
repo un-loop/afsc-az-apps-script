7046dd60b9e32210c0b1b981bdcf0f7e1a353c38
Jessi Shank <jessishank1@gmail.com>
2020-12-15 11:15:58 -0800

    Changed html body to be a function to build email html body

--- Code.ts
+++ Code.ts
@@ -21,37 +21,41 @@ const createOnFormSubmitTrigger = () => {
     .create();
 };
 
-// const createEmailLimitResetTrigger = () => {
-//   ScriptApp.newTrigger('sendConfirmationEmail')
-//   .timeBased()
-//   .everyHours(24)
-//   .create();
-// };
+const buildHTMLBody = (name) => `<!DOCTYPE html>
+<html>
+  <head>
+    <base target="_top">
+  </head>
+  <body>
+    <p>Hi ${name}, </p>
+    <br />
+    <p>Thank you for using the <a href="http://afscarizona.org/send-postcard/">ReFraming Justice Postcard Generator</a> to tell Arizona lawmakers why you support sentencing reform! Be sure to follow AFSC-Arizona on <a href="https://www.facebook.com/AFSCArizona">Facebook</a>, <a href="https://www.instagram.com/afscaz/">Instagram</a> & <a href="https://twitter.com/afscaz">Twitter</a> so you can help amplify our message and stay up-to-date on legislative developments.</p>
+    <br />
+    <p>Stay safe & stay strong!</p>
+    <p>AFSC-Arizona | ReFraming Justice</p>
+  </body>
+</html>`
 
 const EMAIL_SENT = 'EMAIL_SENT';
 const sendConfirmationEmail = (userInfo, rowIndex) => {
   let sheet = SpreadsheetApp.getActiveSheet();
   let emailQuotaRemaining = MailApp.getRemainingDailyQuota();
   Logger.log("Remaining email quota: " + emailQuotaRemaining);
-  // let htmlTemplate = HtmlService.createTemplateFromFile("email");
-  // htmlTemplate.name = userInfo.name;
-  // let htmlBody = htmlTemplate.evaluate().getContent();
   let message = "Thank you for participating " + userInfo.name + ". A postcard will be created in your name and delivered to the legislators.";
   let email_sent = sheet.getRange(rowIndex, 10).getValue();
-    if (email_sent !== EMAIL_SENT) {
-      let subject = "Reframing Justice Project";
-      MailApp.sendEmail(userInfo.email, subject, message, { htmlBody: emailBody });
-      Logger.log('htmlBody to see if coming through at all: ', emailBody);
-      sheet.getRange(rowIndex, 10).setValue(EMAIL_SENT);
-      SpreadsheetApp.flush();
-    };
+  if (email_sent !== EMAIL_SENT) {
+    let subject = "Reframing Justice Project";
+    MailApp.sendEmail(userInfo.email, subject, message, { htmlBody: buildHTMLBody(userInfo.name) });
+    Logger.log('htmlBody to see if coming through at all: ', buildHTMLBody(userInfo.name));
+    sheet.getRange(rowIndex, 10).setValue(EMAIL_SENT);
+    SpreadsheetApp.flush();
   }
 };
 
 const postToLob = (userInfo, rowIndex) => {
   let url = "https://api.lob.com/v1/postcards";
   let data = {
-    description: "Test template for postcard", 
+    description: "Test template for postcard",
       to: toAddress,
     from: null,
     front: '<html style="padding: 1in; font-size: 50;"></html>',
@@ -69,11 +73,11 @@ const postToLob = (userInfo, rowIndex) => {
     contentType: 'application/json',
     headers: {
       Authorization: "Basic " + Utilities.base64Encode(API_KEY + ":"),
-      
+
     },
     payload: JSON.stringify(data),
     muteHttpExceptions: true
-    
+
   };
   Logger.log(data);
   try{
@@ -88,6 +92,6 @@ const postToLob = (userInfo, rowIndex) => {
   } catch (error) {
     Logger.log('error: ', error);
   }
- 
+
 };
 
