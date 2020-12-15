f1c4b9e1966d26edf9cb7d529e22917b15c72584
becky <beckymccarthy777@gmail.com>
2020-12-15 10:51:13 -0800

    Emails confirmation sending and including links

--- Code.ts
+++ Code.ts
@@ -2,12 +2,12 @@ const onFormSubmit = (event: GoogleAppsScript.Events.SheetsOnFormSubmit) => {
   Logger.log(event);
   Logger.log(JSON.stringify(event));
   Logger.log(event.namedValues)
-  var sheet = SpreadsheetApp.getActiveSheet();
-  var rowIndex = event.range.getLastRow();
-  var name = sheet.getRange(rowIndex, 2).getValue();
-  var email = sheet.getRange(rowIndex, 3).getValue();
-  var city = sheet.getRange(rowIndex, 5).getValue();
-  var reason = sheet.getRange(rowIndex, 6).getValue();
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let rowIndex = event.range.getLastRow();
+  let name = sheet.getRange(rowIndex, 2).getValue();
+  let email = sheet.getRange(rowIndex, 3).getValue();
+  let city = sheet.getRange(rowIndex, 5).getValue();
+  let reason = sheet.getRange(rowIndex, 6).getValue();
   const userInfo = {name, email, city, reason};
   Logger.log('userInfo: ', userInfo);
   postToLob(userInfo, rowIndex);
@@ -21,14 +21,27 @@ const createOnFormSubmitTrigger = () => {
     .create();
 };
 
-var EMAIL_SENT = 'EMAIL_SENT';
+// const createEmailLimitResetTrigger = () => {
+//   ScriptApp.newTrigger('sendConfirmationEmail')
+//   .timeBased()
+//   .everyHours(24)
+//   .create();
+// };
+
+const EMAIL_SENT = 'EMAIL_SENT';
 const sendConfirmationEmail = (userInfo, rowIndex) => {
-  var sheet = SpreadsheetApp.getActiveSheet();
-    var message = "Thank you for participating " + userInfo.name + ". A postcard will be created in your name and delivered to the legislators.";
-    var email_sent = sheet.getRange(rowIndex, 10).getValue();
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let emailQuotaRemaining = MailApp.getRemainingDailyQuota();
+  Logger.log("Remaining email quota: " + emailQuotaRemaining);
+  // let htmlTemplate = HtmlService.createTemplateFromFile("email");
+  // htmlTemplate.name = userInfo.name;
+  // let htmlBody = htmlTemplate.evaluate().getContent();
+  let message = "Thank you for participating " + userInfo.name + ". A postcard will be created in your name and delivered to the legislators.";
+  let email_sent = sheet.getRange(rowIndex, 10).getValue();
     if (email_sent !== EMAIL_SENT) {
-      var subject = "Reframing Justice Project";
-      MailApp.sendEmail(userInfo.email, subject, message);
+      let subject = "Reframing Justice Project";
+      MailApp.sendEmail(userInfo.email, subject, message, { htmlBody: emailBody });
+      Logger.log('htmlBody to see if coming through at all: ', emailBody);
       sheet.getRange(rowIndex, 10).setValue(EMAIL_SENT);
       SpreadsheetApp.flush();
     };
@@ -37,13 +50,12 @@ const sendConfirmationEmail = (userInfo, rowIndex) => {
 
 const postToLob = (userInfo, rowIndex) => {
   let url = "https://api.lob.com/v1/postcards";
-  let API_KEY = "test_7116a24dd8a038acadc7bbed8eff2c55168";
   let data = {
     description: "Test template for postcard", 
-      to: "adr_bb8c6abf6bf2c612",
+      to: toAddress,
     from: null,
     front: '<html style="padding: 1in; font-size: 50;"></html>',
-    back: "tmpl_3cdf2f9422f2968",
+    back: back_tmpl,
     merge_variables: {
       name: userInfo.name,
       email: userInfo.email,
@@ -67,10 +79,11 @@ const postToLob = (userInfo, rowIndex) => {
   try{
      // @ts-ignore
     let response = UrlFetchApp.fetch(url, options);
-    var responseCode = response.getResponseCode();
-    var sheet = SpreadsheetApp.getActiveSheet();
-    var values = [[new Date(), responseCode]];
+    let responseCode = response.getResponseCode();
+    let sheet = SpreadsheetApp.getActiveSheet();
+    let values = [[new Date(), responseCode]];
     sheet.getRange(rowIndex, 11, 1, 2).setValues(values);
+    Logger.log('testing options: ', options);
     Logger.log('response: ', response);
   } catch (error) {
     Logger.log('error: ', error);
