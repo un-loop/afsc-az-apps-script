79280dc2c2ad72e4fe7d34548df04414e964f452
becky <beckymccarthy777@gmail.com>
2020-12-18 12:05:30 -0800

    Header mapping finished, cleaned up unused code, pass sheet
    and header instead recreating each time now

--- Code.ts
+++ Code.ts
@@ -2,23 +2,18 @@ import { Header } from './Header';
 
 
 const onFormSubmit = (event: GoogleAppsScript.Events.SheetsOnFormSubmit) => {
-  Logger.log(event);
-  Logger.log(JSON.stringify(event));
-  Logger.log(event.namedValues)
   let sheet = SpreadsheetApp.getActiveSheet();
   let header = Header.fieldToIndex(sheet, requiredFields);
   let rowIndex = event.range.getLastRow();
   let fname = sheet.getRange(rowIndex, header.FIRST_NAME).getValue();
-  // let fname = sheet.getRange(rowIndex, columnIndices.FNAME).getValue();
   let lname = sheet.getRange(rowIndex, header.LAST_NAME).getValue();
   let email = sheet.getRange(rowIndex, header.EMAIL_ADDRESS).getValue();
   let city = sheet.getRange(rowIndex, header.CITY).getValue();
   let reason = sheet.getRange(rowIndex, header.REASON).getValue();
   let include = sheet.getRange(rowIndex, header.EMAIL_INCLUDE).getValue();
   const userInfo = {fname, lname, email, city, reason, include};
-  Logger.log('userInfo: ', userInfo);
-  postToLob(userInfo, rowIndex);
-  sendConfirmationEmail(userInfo, rowIndex);
+  postToLob(userInfo, rowIndex, header, sheet);
+  sendConfirmationEmail(userInfo, rowIndex, header, sheet);
 };
 
 const buildHTMLBody = (fname) => `<!DOCTYPE html>
@@ -38,9 +33,7 @@ const buildHTMLBody = (fname) => `<!DOCTYPE html>
 
 const EMAIL_SENT = 'EMAIL_SENT';
 const QUOTA_EXCEEDED = 'QUOTA_EXCEEDED';
-const sendConfirmationEmail = (userInfo, rowIndex) => {
-  let sheet = SpreadsheetApp.getActiveSheet();
-  let header = Header.fieldToIndex(sheet, requiredFields);
+const sendConfirmationEmail = (userInfo, rowIndex, header, sheet) => {
   let emailQuotaRemaining = MailApp.getRemainingDailyQuota();
 
   // if we max out the quota (100 emails/24 hour period rolling)
@@ -61,15 +54,12 @@ const sendConfirmationEmail = (userInfo, rowIndex) => {
   if (email_sent !== EMAIL_SENT) {
     let subject = "ReFraming Justice Project";
     MailApp.sendEmail(userInfo.email, subject, message, { htmlBody: buildHTMLBody(userInfo.fname) });
-    Logger.log('htmlBody to see if coming through at all: ', buildHTMLBody(userInfo.fname));
     sheet.getRange(rowIndex, header.EMAIL_SENT).setValue(EMAIL_SENT);
     SpreadsheetApp.flush();
   }
 };
 
-const postToLob = (userInfo, rowIndex) => {
-  let sheet = SpreadsheetApp.getActiveSheet();
-  let header = Header.fieldToIndex(sheet, requiredFields);
+const postToLob = (userInfo, rowIndex, header, sheet) => {
   let url = "https://api.lob.com/v1/postcards";
   let data = {
     description: "Postcard",
@@ -77,13 +67,7 @@ const postToLob = (userInfo, rowIndex) => {
     from: null,
     front: front_tmpl,
     back: back_tmpl,
-    merge_variables: {
-      fname: userInfo.fname,
-      lname: userInfo.lname,
-      email: userInfo.email,
-      city: userInfo.city,
-      reason: userInfo.reason,
-    },
+    merge_variables: userInfo,
 
   };
   let options = {
@@ -97,16 +81,12 @@ const postToLob = (userInfo, rowIndex) => {
     muteHttpExceptions: true
 
   };
-  Logger.log(data);
   try{
      // @ts-ignore
     let response = UrlFetchApp.fetch(url, options);
     let responseCode = response.getResponseCode();
-    let sheet = SpreadsheetApp.getActiveSheet();
     let values = [[new Date(), responseCode]];
     sheet.getRange(rowIndex, header.SENT_TO_LOB, 1, 2).setValues(values);
-    Logger.log('testing options: ', options);
-    Logger.log('response: ', response);
   } catch (error) {
     Logger.log('error: ', error);
   }
--- Config.ts
+++ Config.ts
@@ -1,10 +1,8 @@
-
 const toAddress = "adr_6827671dfe59420f";
-// const toAddress = "adr_bb8c6abf6bf2c612";
-const front_tmpl = "tmpl_8cfbad0504a30fb";
-const back_tmpl = "tmpl_3cdf2f9422f2968";
 
-// PropertiesService.getScriptProperties().setProperty("API_KEY", "test_7116a24dd8a038acadc7bbed8eff2c55168");
+const front_tmpl = "tmpl_8cfbad0504a30fb";
+
+const back_tmpl = "tmpl_3cdf2f9422f2968";
 
 const API_KEY = PropertiesService.getScriptProperties().getProperty('API_KEY');
 
@@ -15,16 +13,9 @@ const requiredFields = [
   'CITY',
   'REASON',
   'EMAIL_INCLUDE',
+  'EMAIL_SENT',
+  'SENT_TO_LOB',
+  'STATUS_CODE',
+  'RETRY_COUNT',
+  'IDEMPOTENCY_KEY',
 ]
-
-
-// const columnIndices = {
-//     FNAME: 2,
-//     LNAME: 10,
-//     EMAIL: 3,
-//     CITY: 4,
-//     REASON: 5,
-//     EMAIL_SENT: 11,
-//     SENT_TO_LOB: 12,
-//     STATUS_CODE: 13
-// };
