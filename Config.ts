af81dfe6f234e3197ef3178355aef7cac701e05d
Jessi Shank <jessishank1@gmail.com>
2021-01-11 12:38:15 -0800

    Updating lob ids

--- Config.ts
+++ Config.ts
@@ -1,8 +1,8 @@
-const toAddress = "adr_6827671dfe59420f";
+const toAddress = "adr_763686ed73dd97fc";
 
-const front_tmpl = "tmpl_8cfbad0504a30fb";
+const front_tmpl = "tmpl_86d56082cce5ae7";
 
-const back_tmpl = "tmpl_3cdf2f9422f2968";
+const back_tmpl = "tmpl_1d8366980e4d87c";
 
 const API_KEY = PropertiesService.getScriptProperties().getProperty('API_KEY');
 
